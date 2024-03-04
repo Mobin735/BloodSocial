4abf4654c84a12bbe620fd2b800490bdc0c46f99
Mobin735 <mobindesai092@gmail.com>
2024-03-05 02:48:05 +0530

    new

--- Frontend/src/utils/VerifyUser.js
+++ Frontend/src/utils/VerifyUser.js
@@ -18,7 +18,7 @@ const VerifyUser = async () => {
         console.log("Sending req to server");
         try {
             // sending token to server via cookie to verify 
-            const response = await axios.get(`${process.env.REACT_APP_API}/auth/verify`, {
+            const response = await axios.get(`${process.env.REACT_APP_API}/auth/verify`,null,{
                 withCredentials: true,
                 mode: 'cors',
             });
