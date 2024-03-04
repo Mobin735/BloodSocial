f9e2bc58faee6c0d458b38e2751c9e3d592fd90d
Mobin735 <mobindesai092@gmail.com>
2024-03-05 03:43:11 +0530

    try

--- Backend/routes/verifyuser.js
+++ Backend/routes/verifyuser.js
@@ -4,6 +4,10 @@ import { VerifyJWT } from "../utils/JwtToken.js";
 const verifyuser = express.Router();
 
 verifyuser.get('/verify',VerifyJWT,(req, res) => {
+    res.header('Access-Control-Allow-Origin', 'https://blood-social.vercel.app');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Credentials', 'true');
     const userData = {
         email: req.data.user.email,
         fullname: req.data.user.fullname,
--- Backend/utils/JwtToken.js
+++ Backend/utils/JwtToken.js
@@ -17,12 +17,16 @@ export function CreateJWT(data) {
 };
 
 export function VerifyJWT(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'https://blood-social.vercel.app');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Credentials', 'true');
     const authHeader = req.headers.cookie;
-    console.log("AuthHeader: "+authHeader);
+    console.log("AuthHeader: " + authHeader);
     if (typeof authHeader !== 'undefined') {
         const cookies = authHeader.split("; ");
         const accessTokenCookie = cookies.find(cookie => cookie.startsWith('access_token='));
-        
+
         if (accessTokenCookie) {
             const token = accessTokenCookie.split(" ")[1];
             try {
