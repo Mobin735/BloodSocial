22b11c656a488897fe16ddb99ca039a897b8b55b
Mobin735 <mobindesai092@gmail.com>
2024-03-05 15:00:54 +0530

    one approach

--- Backend/index.js
+++ Backend/index.js
@@ -20,20 +20,8 @@ const corsOptions = {
 dotenv.config();
 const app = express();
 
-// app.use(cors({
-//   origin: 'https://blood-social.vercel.app',
-//   methods: ["POST","GET"], // Replace with your actual origin
-//   credentials: true,
-// }));
-
 app.use(cors(corsOptions));
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', allowedOrigins);
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  next();
-});
+
 
 app.use(express.json());
 app.use(cookieParser());
