8eff806425c0589d870b2f48199946134c837090
Mobin735 <mobindesai092@gmail.com>
2024-03-05 14:15:31 +0530

    new

--- Backend/utils/JwtToken.js
+++ Backend/utils/JwtToken.js
@@ -18,7 +18,7 @@ export function CreateJWT(data) {
 
 export function VerifyJWT(req, res, next) {
     const authHeader = req.headers.cookie;
-    // console.log("AuthHeader: " + authHeader);
+    console.log("AuthHeader bin: " + authHeader);
     if (typeof authHeader !== 'undefined') {
         const cookies = authHeader.split("; ");
         const accessTokenCookie = cookies.find(cookie => cookie.startsWith('access_token='));
