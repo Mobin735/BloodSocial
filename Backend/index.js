1d8f93957e1d2c1fa5ced54464d8ce7d7b2299bf
Mobin735 <mobindesai092@gmail.com>
2024-03-05 14:12:35 +0530

    test

--- Backend/index.js
+++ Backend/index.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', allowedOrigins);
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Credentials', 'false');
   next();
 });
 
